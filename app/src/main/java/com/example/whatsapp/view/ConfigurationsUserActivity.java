8fc30a804e33712a5cff78e09f9150175dd150cb
Marc8s <marcus.ecomp@gmail.com>
2021-04-09 20:48:36 +0100

    Configurando escolha de fotos

--- app/src/main/java/com/example/whatsapp/view/ConfigurationsUserActivity.java
+++ app/src/main/java/com/example/whatsapp/view/ConfigurationsUserActivity.java
@@ -7,25 +7,36 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.whatsapp.R;
 import com.example.whatsapp.helper.Permission;
 
 public class ConfigurationsUserActivity extends AppCompatActivity {
 
-    private String[] permissionsNeeded = new String[]{
+    private String[] mPermissionsNeeded = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+    private ImageButton mImageButtonCamera, mImageButtonGallery;
+    private static final int mSELECTION_CAMERA = 100;
+    private static final int mSELECTION_GALLERY = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurations_user);
 
-        Permission.validatePermissions(permissionsNeeded, this, 1);
+        Permission.validatePermissions(mPermissionsNeeded, this, 1);
+
+        mImageButtonCamera = findViewById(R.id.imageButtonCamera);
+        mImageButtonGallery = findViewById(R.id.imageButtonGallery);
 
         Toolbar toolbar = findViewById(R.id.toolbarMain);
         toolbar.setTitle(R.string.config);
@@ -33,6 +44,24 @@ public class ConfigurationsUserActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //alterar suporte bar adicionando o botão voltar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mImageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(i.resolveActivity(getPackageManager())!= null){
+                    startActivityForResult(i, mSELECTION_CAMERA);
+                }
+
+            }
+        });
+
+        mImageButtonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
